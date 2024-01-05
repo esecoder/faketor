b1e8cc0e64ae46bc70f8ebbcbd7abdfa906215a0
Ese Udom <udomese@yahoo.com>
2024-01-05 17:41:04 +0100

    Add Generics

--- src/generator/customAugmenter.ts
+++ src/generator/customAugmenter.ts
@@ -2,12 +2,30 @@ import { Column } from "./schema/column";
 
 /** This is the special data to be added to a table outside of the generated ones during insertion query. 
  * Column is the column for that data */
-export class CustomAugmenter {
-    augmentationData: string[];
+
+//Enforce it to only accept string[] or Map<string, string> types.
+type StringArrayOrMap<T> = T extends string[] ? string[] : T extends Map<string, string> ? Map<string, string> : never;
+
+export class CustomAugmenter<T extends string[] | Map<string, string>> {
+    augmentationData: StringArrayOrMap<T>;
     column: Column;
 
-    public constructor(augmentationData: string[], column: Column) {
+    public constructor(augmentationData: StringArrayOrMap<T>, column: Column) {
         this.augmentationData = augmentationData;
         this.column = column;
     }
+
+    getValue(): StringArrayOrMap<T> {
+        return this.augmentationData;
+    }
+
+    setValue(value: StringArrayOrMap<T>): void {
+        this.augmentationData = value;
+    }
+
+    //private value: StringArrayOrMap<T>;
+
+    /*constructor(value: StringArrayOrMap<T>) {
+        this.value = value;
+    }*/
 }
--- src/generator/customData.ts
+++ src/generator/customData.ts
@@ -2,12 +2,24 @@ import { Column } from "./schema/column";
 
 /** This is the special data to be added to a table outside of the generated ones during insertion query. 
  * Column is the column for that data */
-export class CustomData {
-    customData: string[];
+
+//Enforce it to only accept string[] or Map<string, string> types.
+type StringArrayOrMap<T> = T extends string[] ? string[] : T extends Map<string, string> ? Map<string, string> : never;
+
+export class CustomData<T extends string[] | Map<string, string>> {
+    customData: StringArrayOrMap<T>;
     column: Column;
 
-    public constructor(customData: string[], column: Column) {
+    public constructor(customData: StringArrayOrMap<T>, column: Column) {
         this.customData = customData;
         this.column = column;
+    }
+
+    getValue(): StringArrayOrMap<T> {
+        return this.customData;
+    }
+
+    setValue(value: StringArrayOrMap<T>): void {
+        this.customData = value;
     }
 }
